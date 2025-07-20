c617e22fd64da226a147544786461c1fc4c8f49f
Mauricio7015 <46784110+Mauricio7015@users.noreply.github.com>
2025-07-20 18:50:54 -0300

    Add simple SQLite migration

--- main.py
+++ main.py
@@ -2,7 +2,9 @@
 
 from bot.scheduler import start as start_scheduler
 from bot.bot import start_bot
+from bot.database import migrate
 
 if __name__ == '__main__':
+    migrate()
     start_scheduler()
     start_bot()
