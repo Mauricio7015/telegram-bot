cf7df854591df1d904447b996104aa0da7a156aa
Mauricio7015 <46784110+Mauricio7015@users.noreply.github.com>
2025-07-20 17:46:30 -0300

    Fix APScheduler timezone error

--- bot/scheduler.py
+++ bot/scheduler.py
@@ -1,15 +1,16 @@
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.triggers.cron import CronTrigger
+import pytz
 
 from .database import get_session
 from .poster import send_public_post, send_private_post
 from .config import CRON_HOUR
 
-scheduler = BackgroundScheduler()
+scheduler = BackgroundScheduler(timezone=pytz.utc)
 
 
 def start():
-    trigger = CronTrigger(hour=CRON_HOUR, minute=0)
+    trigger = CronTrigger(hour=CRON_HOUR, minute=0, timezone=pytz.utc)
     scheduler.add_job(lambda: send_public_post(get_session()), trigger)
     scheduler.add_job(lambda: send_private_post(get_session()), trigger)
     scheduler.start()
